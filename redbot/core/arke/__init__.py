f5f78a431a6df54c260e8fdbcf6f7ef3cba372c7
retke <laggron42@gmail.com>
2022-07-11 11:54:00 +0200

    init: import everything from tortoise

--- redbot/core/arke/__init__.py
+++ redbot/core/arke/__init__.py
@@ -1,12 +1,30 @@
 from redbot.core.errors import CogLoadError
 
 try:
-    from tortoise import *
+    import tortoise
 except ImportError as e:
     raise CogLoadError(
         "You need to install Red with the `arke` dependency to use this feature. "
         "Run `python3 -m pip install -U Red-DiscordBot[arke]`."
     )
+
+from tortoise import backends
+from tortoise import contrib
+from tortoise import converters
+from tortoise import exceptions
+from tortoise import expressions
+from tortoise import filters
+from tortoise import functions
+from tortoise import indexes
+from tortoise import manager
+from tortoise import models
+from tortoise import query_utils
+from tortoise import queryset
+from tortoise import signals
+from tortoise import timezone
+from tortoise import transactions
+from tortoise import utils
+from tortoise import validators
 
 from redbot.core.arke.manager import *
 from redbot.core.arke.meta import *
